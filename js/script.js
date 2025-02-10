2571aea587507d701cf8edb6baec5f9a8b0a10b2
jonathanjogy <jonathan.grieshofer@gmail.com>
2025-02-10 09:49:34 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
-const image = document.querySelector('.sirupfam');
-const text = document.querySelector('.text-fam');
+const sirupfam = document.querySelector('.sirupfam');
+const textfam = document.querySelector('.text-fam');
+const scrollup = document.querySelector('.colorflower');
+const sirupfamsmal = document.querySelector ('.sirupfamsmal');
+const textfamsmall = document.querySelector ('.text-famsmall');
 
-image.addEventListener('click', function() {
-    image.classList.toggle('large');
-	text.classList.toggle('show');
+
+
+sirupfam.addEventListener('mouseover', function() {
+    sirupfam.classList.toggle('large');
+	textfam.classList.toggle('show');
 });
 
+sirupfam.addEventListener('mouseout', function() {
+    sirupfam.classList.toggle('large');
+	textfam.classList.toggle('show');
+});
+
+sirupfamsmal.addEventListener('mouseover', function() {
+    sirupfamsmal.classList.toggle('large');
+	textfamsmall.classList.toggle('show');
+});
+
+sirupfamsmal.addEventListener('mouseout', function() {
+    sirupfamsmal.classList.toggle('large');
+	textfamsmall.classList.toggle('show');
+});
+
+scrollup.addEventListener('mouseover', function () {
+	scrollup.classList.toggle('press');
+	window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
